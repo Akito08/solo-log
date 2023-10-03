17b31804561ca8df4d7aa5479c9190df18ac4ae8
Akito08 <sza9ki8to7@gmail.com>
2023-10-03 19:32:20 +0900

    add controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :shop
 
   validates :rate, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :comment, length: { maximum: 1500 }
   validates :counter_sheets_available, allow_blank: true
   validates :solo_tables_available, allow_blank: true
   validates :frequent_solo_visitors, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, allow_blank: true }
